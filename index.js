c75eceb4065453c26f83e57458edc56197cba879
Joe P Shoulak <joe@squarecompass.io>
2022-10-07 13:55:44 -0500

    Someone plz help lol

--- index.js
+++ index.js
@@ -1,87 +1,58 @@
 import inquirer from "inquirer";
+import Intern from "./lib/Intern.js";
 import Engineer from "./lib/Engineer.js";
 import Manager from "./lib/Manager.js";
 
-const employeeQuestions = [
-    {
-        "name": "name",
-        "type": "input",
-        "message": "Name:"
-    },
-    {
-        "name": "id",
-        "type": "input",
-        "message": "ID:"
-    },
-    {
-        "name": "email",
-        "type": "input",
-        "message": "Email:"
-    },
-];
-
-const managerQuestions = [...employeeQuestions, {
-    "name": "officeNumber",
-    "type": "input",
-    "message": "Office Number:"
-}];
-
-const engineerQuestions = [...employeeQuestions, {
-    "name": "gh_username",
-    "type": "input",
-    "message": "GitHub Username:"
-}];
-
-const internQuestions = [...employeeQuestions, {
-    "name": "school",
-    "type": "input",
-    "message": "School:"
-}];
-
-const loopQuestions = [{
-    "name": "choice",
-    "type": "list",
-    "message": "Next:",
-    "choices": [ "Add Engineer", "Add Intern", "End" ]
-}]
+import {loopQuestions, internQuestions, managerQuestions, engineerQuestions} from "./lib/questions.js"
 
 console.log("\nEnter Manager Information:\n");
 
 let employees = [];
+
+function addEngineer(employees) {
+    inquirer
+        .prompt(engineerQuestions)
+        .then((answers) => {
+            employees = [...employees, new Engineer(answers)];
+        });
+
+    return employees;
+}
+
+function addIntern(employees) {
+    inquirer
+        .prompt(internQuestions)
+        .then((answers) => {
+            employees = [...employees, new Intern(answers)];
+        })
+
+    return employees;
+}
+
+function addNextEmployee(employees) {
+    inquirer
+    .prompt(loopQuestions)
+    .then((answers) => {
+        switch (answers.choice) {
+            case "Add Engineer":
+                // TODO: Figure out why this isn't working as expected
+                addEngineer(employees);
+                addNextEmployee(employees)
+                break;
+            case "Add Intern":
+                addIntern(employees);
+                addNextEmployee(employees)
+                break;
+            case "End":
+                break;
+        }
+    })
+}
 
 inquirer
     .prompt(managerQuestions)
     .then((answers) => {
         employees = [...employees, new Manager(answers)];
 
-        // TODO: Figure out how to get this working
-        // let choice;
-        // while (choice != "End"){
-
-            inquirer
-                .prompt(loopQuestions)
-                .then(({choice}) => {
-                    switch (choice) {
-                        case "Add Engineer":
-                            inquirer
-                                .prompt(engineerQuestions)
-                                .then((answers) => {
-                                    employees = [...employees, new Engineer(answers)];
-                                })
-                            break;
-                        case "Add Intern":
-                            inquirer
-                                .prompt(internQuestions)
-                                .then((answers) => {
-                                    employees = [...employees, new Intern(answers)];
-                                })
-                            break;
-                        case "End":
-                            // Generate the HTML and save the file
-                            break;
-                    }
-                })
-
-        // }
-
+        addNextEmployee(employees);
     });
