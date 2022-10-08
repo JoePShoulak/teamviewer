1f245cccd6109b28fe3d5bc74b23ee7a8be9b069
Joe P Shoulak <joe@squarecompass.io>
2022-10-07 19:24:35 -0500

    update src/example

--- index.js
+++ index.js
@@ -52,7 +52,8 @@ function generateWebpage(employees) {
         card = card.replace("ROLE", role);
         card = card.replace("NAME", employee.getName());
         card = card.replace("ID", `ID: ${employee.getID()}`);
-        card = card.replace("EMAIL", `Email: ${employee.getEmail()}`);
+        // TODO: Make email be a link
+        card = card.replace("EMAIL", `Email: <a href="mailto:${employee.getEmail()}">${employee.getEmail()}</a>`);
 
         switch (role) {
             case "Manager":
